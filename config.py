a41be143274fb30b4f5df4dfcb8952cc229b2381
VIVEK <99870677+Roninopp@users.noreply.github.com>
2022-09-20 23:56:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,5 +17,5 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/12245022cf675d057b79e.
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/12245022cf675d057b79e.jpg")
 SESSION_NAME = getenv("SESSION_NAME", "BQC7YeVY_dfntSCNJaDQl0IZiep5IbEIaiQZ3SOxFDQw3tv-doVV8d89qzQn7oo1kM0n7C_CEC_H1UAko02i3w5vHqQtxDgctu70rczR7-WYoyjy7RiGayatWHpyDcTUmVDV94K1-CL3jnuNGKcNjtGcPibOgqf4C166SJ_UsDCNz69T9WfmTpNZc1GUiQS1TFzIAe3PkDZL-S48FJOWvDR8KfvtPqzE8X9FVXXmVNynEVRiu9NmiX9gy5tQ8_-IYhifCw_1fQdYhlwjeoogAejwmYf0P5fI6QwFeSIbyvnLoTXfVOqBrOxnKQFzAE7MgGjOHLbMd5sKJchjEnAMO2J7AAAAAS3VP2UA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "? ~ + • / ! ^ .").split())
-PMPERMIT = getenv("PMPERMIT", "ENABLE")
+PMPERMIT = getenv("PMPERMIT", "DISABLE")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1793699293").split()))
